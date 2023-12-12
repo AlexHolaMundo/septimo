8bfc11446fa910a668d54306a72a38dfc1559584
AlexHolaMundo <walter.tipanluisa5714@utc.edu.ec>
2023-12-12 15:35:51 -0500

    add form crud

--- tienda/Aplicaciones/Facturacion/views.py
+++ tienda/Aplicaciones/Facturacion/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Cliente
 # Create your views here.
 def home(request):
     clientesBdd=Cliente.objects.all()
     return render (request, 'listadoClientes.html', {'clientes': clientesBdd})
+
+def guardarCliente (request):
+    cedula=request.POST['cedula']
+    nombre=request.POST['nombre']
+    apellido=request.POST['apellido']
+    fechas_nacimiento=request.POST['fechas_nacimiento']
+    correo=request.POST['correo']
+    direccion=request.POST['direccion']
+    nuevoCliente=Cliente.objects.create(cedula=cedula, nombre=nombre, apellido=apellido, fechas_nacimiento=fechas_nacimiento, correo=correo, direccion=direccion)
+    return redirect  ('/')
