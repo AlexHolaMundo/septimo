453e2fb37ad27b61fc3b16163a1fe1dbcd52dee1
AlexHolaMundo <walter.tipanluisa5714@utc.edu.ec>
2024-01-04 16:49:55 -0500

    add relaciones entre tablas

--- tienda/Aplicaciones/Facturacion/models.py
+++ tienda/Aplicaciones/Facturacion/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Tipo(models.Model):
+    id=models.AutoField(primary_key=True)
+    nombre=models.CharField(max_length=150)
+    descripcion=models.TextField(max_length=200)
+    def __str__(self):
+        fila="{0} => {1} => {2}"
+        return fila.format(self.id,self.descripcion, self.nombre)
 class Cliente(models.Model):
     id=models.AutoField(primary_key=True)
     cedula=models.CharField(max_length=10)
@@ -9,6 +16,7 @@ class Cliente(models.Model):
     direccion=models.TextField(max_length=100)
     fechas_nacimiento=models.DateField()
     correo=models.EmailField()
+    tipo=models.ForeignKey(Tipo, null=True, blank=True, on_delete=models.PROTECT)
     def __str__(self):
-        fila="{0} => {1} => {2} => {3} => {4} => {5} => {6}"
-        return fila.format(self.id,self.cedula,self.apellido,self.nombre,self.direccion,self.fechas_nacimiento,self.correo)
+        fila="{0} => {1} => {2} => {3} => {4} => {5} => {6} => {7}"
+        return fila.format(self.id, self.tipo,self.cedula,self.apellido,self.nombre,self.direccion,self.fechas_nacimiento,self.correo)
--- tienda/Aplicaciones/Facturacion/views.py
+++ tienda/Aplicaciones/Facturacion/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render, redirect
-from .models import Cliente
+from .models import Cliente, Tipo
 from django.contrib import messages
 # Create your views here.
 def home(request):
     clientesBdd=Cliente.objects.all()
-    return render (request, 'listadoClientes.html', {'clientes': clientesBdd})
+    tipoBdd=Tipo.objects.all()
+    return render (request, 'listadoClientes.html', {'clientes': clientesBdd , 'tipos': tipoBdd})
 
 def guardarCliente (request):
+    tipo=request.POST['id_tipo']
+    tipoSeleccionado=Tipo.objects.get(id=tipo)
     cedula=request.POST['cedula']
     nombre=request.POST['nombre']
     apellido=request.POST['apellido']
     fechas_nacimiento=request.POST['fechas_nacimiento']
     correo=request.POST['correo']
     direccion=request.POST['direccion']
-    nuevoCliente=Cliente.objects.create(cedula=cedula, nombre=nombre, apellido=apellido, fechas_nacimiento=fechas_nacimiento, correo=correo, direccion=direccion)
+    nuevoCliente=Cliente.objects.create(cedula=cedula, nombre=nombre, apellido=apellido, fechas_nacimiento=fechas_nacimiento, correo=correo, direccion=direccion,tipo=tipoSeleccionado,)
     messages.success(request, 'Cliente guardado correctamente')
     return redirect  ('/')
 
@@ -25,18 +28,23 @@ def eliminarCliente (request, id):
 
 def editarCliente (request, id):
     clienteEditar=Cliente.objects.get(id=id)
-    return render (request, 'editarCliente.html', {'cliente': clienteEditar})
+    tipoBdd=Tipo.objects.all()
+    return render (request, 'editarCliente.html', {'cliente': clienteEditar, 'tipos': tipoBdd})
 
 def procesarActualizacionCliente (request):
     id=request.POST['id']
+    id_tipo=request.POST['id_tipo']
+    tipoSeleccionado=Tipo.objects.get(id=id_tipo)
     cedula=request.POST['cedula']
     nombre=request.POST['nombre']
     apellido=request.POST['apellido']
     fechas_nacimiento=request.POST['fechas_nacimiento']
     correo=request.POST['correo']
     direccion=request.POST['direccion']
+    # Actualizar el cliente
     cliente=Cliente.objects.get(id=id)
     cliente.cedula=cedula
+    cliente.tipo=tipoSeleccionado
     cliente.nombre=nombre
     cliente.apellido=apellido
     cliente.fechas_nacimiento=fechas_nacimiento
